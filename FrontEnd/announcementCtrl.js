dea72765fe1ded79ab47a25ed0c055345e63f6b8
Neha Sachdev <nehasach@usc.edu>
2017-09-28 11:15:00 -0700

    Pushed my all changes - Frontent Prof Page

--- FrontEnd/announcementCtrl.js
+++ FrontEnd/announcementCtrl.js
@@ -1,4 +1,14 @@
 ucr.controller("announcementCtrl",["$scope","$http",function($scope,$http){
 	console.log("announcementCtrl");
 
+$scope.Course1 = [
+   {courseId:'CSCI585',courseName:'Database Systems'},
+   {courseId:'CSCI586',courseName:'Database Systems Interoperability'},
+   {courseId:'CSCI571',courseName:'Web Technologies'},
+   {courseId:'CSCI572',courseName:'Information Retrieval & Web Search Engines'}];
+
+$scope.DisplayForm = function(id){
+    alert("Neha");
+}
+
 }]);
---
+++ FrontEnd/courseReviewCtrl.js
@@ -0,0 +1,4 @@
+ucr.controller("courseReviewCtrl",["$scope","$http",function($scope,$http){
+	console.log("courseReviewCtrl");
+
+}]);
